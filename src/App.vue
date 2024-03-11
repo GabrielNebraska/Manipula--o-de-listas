a51fb1dd20f1cb3b0da6613a6392371d99da053a
GabrielNebraska <gabrielviniciuscorrea204@gmail.com>
2024-03-11 11:53:52 -0300

    Manipulação de Listas

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+const items = [
+  { id: '1', name: 'item1' },
+  { id: '2', name: 'item2' },
+  { id: '3', name: 'item3' }
+]
+</script>
+<template>
+  <div v-for="item in items" :key="item.id">
+    {{ item.name }}>
+    <p>{{ item.name }}</p>
+  </div>
+</template>
